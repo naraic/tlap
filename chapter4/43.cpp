22abe537deab7943b9381ff7d9b6b15b108b5c0a
Ciarán <naraic0o@gmail.com>
2017-07-30 17:41:27 +0100

    thought i was making 4.3 better. now i am not sure - almost working, using no char array indexes

--- chapter4/43.cpp
+++ chapter4/43.cpp
@@ -5,39 +5,82 @@ using std::endl;
 typedef char * arrayString ;
 
 int arrayStringLen(arrayString s); 	
-void replaceString(arrayString source, arrayString target, arrayString replaceText);
+int replaceFirst(arrayString& haystack, arrayString needle, arrayString pin);
+void replaceString(arrayString& source, arrayString target, arrayString replaceText);
 
 int main() {
-	arrayString s = new char[9];
-	s[0] = 'a'; s[1] = 'b'; s[2] = 'c'; s[3] = 'd'; s[4] = 'a'; s[5] = 'b'; s[6] = 'e'; s[7] = 'e'; s[8] = 0;
-	arrayString t = new char[3];
-	t[0] = 'a'; t[1] = 'b'; t[2] = 0;
-	arrayString r = new char[4];
-	r[0] = 'x'; r[1] = 'y'; r[2] = 'z'; r[3] = 0;
-	cout << s << endl;
-	replaceString(s, t, r);
-	cout << s << endl;
+    arrayString s = new char[9];
+    s[0] = 'a'; s[1] = 'b'; s[2] = 'c'; s[3] = 'd'; s[4] = 'a'; s[5] = 'b'; s[6] = 'e'; s[7] = 'e'; s[8] = 0;
+    arrayString t = new char[3];
+    t[0] = 'a'; t[1] = 'b'; t[2] = 0;
+    arrayString r = new char[4];
+    r[0] = 'x'; r[1] = 'y'; r[2] = 'z'; r[3] = 0;
+    cout << s << endl;
+    replaceString(s, t, r);
+    cout << s << endl;
 }
 
-void replaceString(arrayString source, arrayString target, arrayString replaceText) {
-	int lenS = arrayStringLen(source);
-	int lenT = arrayStringLen(target);
-	int lenR = arrayStringLen(replaceText);
-	int count = findMatches(source, target);
-	cout << i << endl;
+void replaceString(arrayString& source, arrayString target, arrayString replaceText) {
+    while(replaceFirst(source, target, replaceText))
+        ;
 }
-	
+
 int arrayStringLen(arrayString s) {	
-	int lenS;
-	for (lenS = 0; s[i] != 0; lenS++);
-	return lenS;
+    int i;
+    for (i = 0; s[i] != 0; i++);
+    return i;
 }
 
-int findMatches(arrayString haystack, arrayString needle) {
-    int count = 0;
-	for(int i = 0; haystack[i] != 0; i++) {
-        (int j = 0; needle[j] != 0; j++) {
-            
-            
+int replaceFirst(arrayString& haystack, arrayString needle, arrayString pin) {
+    arrayString newS, newS_ptr; 
+    arrayString haystack_ptr, needle_ptr, haystack_backup;
+    haystack_backup = haystack;
+    haystack_ptr = haystack;
+    while(*haystack_ptr) {
+        needle_ptr = needle;
+        while(*needle_ptr == *haystack_ptr) {
+            needle_ptr++;
+            haystack_ptr++;
+        }
+        if (!*needle_ptr) {
+            newS = new char[ arrayStringLen(haystack) -
+                             arrayStringLen(needle) +
+                             arrayStringLen(pin)];
+            newS_ptr = newS;
+            while(&(*haystack) != &(*haystack_ptr)) {
+                    *newS = *haystack;
+                    newS++;
+                    haystack++;
+            }
+            *newS = 0;
+            cout << newS_ptr << endl;
+            needle_ptr = needle;
+            while(*needle_ptr) {
+                haystack++;
+                needle_ptr++;
+            }
+            while(*pin) {
+                *newS = *pin;
+                newS++;
+                pin++;
+            }
+            *newS = 0;
+            cout << newS_ptr << endl;
+            while(*haystack) {
+                *newS = *haystack;
+                newS++;
+                haystack++;
+            }
+            *newS = 0;
+            cout << newS_ptr << endl;
 
-
+            delete[] haystack_backup;
+            haystack = newS_ptr;
+            return 1;
+        }
+        else {
+            haystack_ptr++;
+        }
+    }
+    return 0;
+}
