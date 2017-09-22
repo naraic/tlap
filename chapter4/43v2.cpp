b687dc81fa5cb6c37b53cf8ff919ffae4a709d80
naraic <naraic0o@gmail.com>
2017-09-22 15:58:05 +0100

    43v2 wip what's there works

--- chapter4/43v2.cpp
+++ chapter4/43v2.cpp
@@ -6,40 +6,55 @@ typedef char * arrayString ;
 
 int arrayStringLen(arrayString s); 	
 void replaceString(arrayString source, arrayString target, arrayString replaceText);
+int countMatches(arrayString haystack, arrayString needle);
 
 int main() {
-	arrayString s = new char[9];
-	s[0] = 'a'; s[1] = 'b'; s[2] = 'c'; s[3] = 'd'; s[4] = 'a'; s[5] = 'b'; s[6] = 'e'; s[7] = 'e'; s[8] = 0;
-	arrayString t = new char[3];
-	t[0] = 'a'; t[1] = 'b'; t[2] = 0;
-	arrayString r = new char[4];
-	r[0] = 'x'; r[1] = 'y'; r[2] = 'z'; r[3] = 0;
-	cout << s << endl;
-	replaceString(s, t, r);
-	cout << s << endl;
+    arrayString s = new char[9];
+    s[0] = 'a'; s[1] = 'b'; s[2] = 'c'; s[3] = 'd'; s[4] = 'a'; s[5] = 'b'; s[6] = 'e'; s[7] = 'e'; s[8] = 0;
+    arrayString t = new char[3];
+    t[0] = 'a'; t[1] = 'b'; t[2] = 0;
+    arrayString r = new char[4];
+    r[0] = 'x'; r[1] = 'y'; r[2] = 'z'; r[3] = 0;
+    cout << s << endl;
+    replaceString(s, t, r);
+    cout << s << endl;
 }
 
 void replaceString(arrayString source, arrayString target, arrayString replaceText) {
-	int lenS = arrayStringLen(source);
-	int lenT = arrayStringLen(target);
-	int lenR = arrayStringLen(replaceText);
-	int count = findMatches(source, target);
-	cout << i << endl;
-}
-	
-int arrayStringLen(arrayString s) {	
-	int lenS;
-	for (lenS = 0; s[i] != 0; lenS++);
-	return lenS;
+    int lenS = arrayStringLen(source);
+    int lenT = arrayStringLen(target);
+    int lenR = arrayStringLen(replaceText);
+    int count = countMatches(source, target);
+    cout << count << endl;
 }
 
-int findMatches(arrayString haystack, arrayString needle) {
+int arrayStringLen(arrayString s) {	
+    int lenS;
+    for (lenS = 0; s[lenS] != 0; lenS++);
+    return lenS;
+}
+
+int countMatches(arrayString haystack, arrayString needle) {
     int count = 0;
-	for(int i = 0; haystack[i] != 0; i++) {
-        for (int j = 0; needle[j] != 0; j++) {
-            while (needle[i] == needle[j])
+    int lenN = arrayStringLen(needle);
+    int last_possible = arrayStringLen(haystack) - lenN;
+    for(int i = 0; i <= last_possible; i++) {
+        int j = 0;
+        while(haystack[i+j] == needle[j] && needle[j] != 0) {
+            j++;
+        }
+        if(j == lenN)
+            count++;
+    }
+    return count;
+}
+
+
+        
             
-            
-            
+
+
+
+
 
 
